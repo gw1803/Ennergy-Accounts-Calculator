7fb2dd77f7ecfb8d5bfe81be5ff69db736c3f8b5
gabriel <gw180304@gmail.com>
2024-06-14 22:42:05 -0300

    feat: Controle de entrada em TextInputs e controle do Disable do botão Calcular na tela de Realizar Avaliação

--- main.py
+++ main.py
@@ -7,8 +7,37 @@ class Inicio(Screen):
     pass
 
 class RealizarCalculo(Screen):
-    pass 
-    
+    padrao_geral = ""
+    padrao_wilson = ""
+    padrao_celio = ""
+    valor_conta = ""
+    valor_kw = ""
+    taxa_iluminacao = ""
+
+    def on_text(self, input):  
+        match input:
+            case self.ids.padrao_geral :
+                self.padrao_geral = self.ids.padrao_geral.text
+            case self.ids.padrao_wilson :
+                self.padrao_wilson = self.ids.padrao_wilson.text
+            case self.ids.padrao_celio :
+                self.padrao_celio = self.ids.padrao_celio.text
+            case self.ids.valor_conta :
+                self.valor_conta = self.ids.valor_conta.text
+            case self.ids.valor_kw :
+                self.valor_kw = self.ids.valor_kw.text
+            case self.ids.taxa_iluminacao:
+                self.taxa_iluminacao = self.ids.taxa_iluminacao.text
+        self.verify_inputs()
+        
+
+    def verify_inputs(self):
+        if (self.padrao_geral != "" )& (self.padrao_wilson != "") & (self.padrao_celio!="") & (self.valor_conta != "") & (self.valor_kw != ""):
+            self.ids.button.disabled = False
+        else:
+            self.ids.button.disabled = True
+ 
+        
 class WindowManager(ScreenManager):
     pass
 
